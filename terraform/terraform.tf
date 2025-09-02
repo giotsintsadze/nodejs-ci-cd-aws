2561b7bda7e2f53e621c59fe8411df576f0d6b0f
gtsintsadze <giotsintsadze94@gmail.com>
2025-09-02 04:24:23 +0400

    removed backend in tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 1.3.0"
 
-  backend "local" {
-    path = "/home/giorgi/projects/nodejs-ci-cd-aws/terraform/terraform.tfstate"
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
