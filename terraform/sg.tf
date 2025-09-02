20bfdf9b2a7895413090d7bf5eb0adc6a89bf79f
Giorgi Tsintsadze <giotsintsadze94@gmail.com>
2025-09-02 00:54:51 +0000

    fmt

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -3,9 +3,9 @@ resource "aws_security_group" "internet" {
   vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
