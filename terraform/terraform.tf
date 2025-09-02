cbd7a93f37183a8afc2e520b2f4574bcdc6f8027
gtsintsadze <giotsintsadze94@gmail.com>
2025-09-02 04:48:49 +0400

    tesiting s3 tfstate

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.3.0"
 
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-teeest"
+    key    = "terraform.tfstate"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
